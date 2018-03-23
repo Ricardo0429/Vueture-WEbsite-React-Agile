55cb2ce7c5c91d9e9a124e31cf075569f9d91566
Sam Joseph <tansaku@gmail.com>
2018-03-23 11:42:58 +0000

    use bootstrap to alternate the boxes left and right

--- src/App.js
+++ src/App.js
@@ -18,27 +18,27 @@ class App extends Component {
         <AVNavbar/>
 
         <ListGroup>
-          <div class="section-well col-sm-8 pull-right">
+          <div class="section-well col-sm-offset-1 col-sm-4">
           <ListGroupItem header="You are here" href="#" bsClass="list-group-item landing-page-markers">
               Tired of toy projects, tutorials, and online courses?
           </ListGroupItem>
           </div>
-          <div class="section-well col-sm-8 pull-right">
+          <div class="section-well col-sm-offset-7 col-sm-4">
           <ListGroupItem header="Standups" href="#" bsClass="list-group-item landing-page-markers">
             Sitting down at home, or not, meet other people in our online hangouts.
           </ListGroupItem>
           </div>
-          <div class="section-well col-sm-8 pull-right">
+          <div class="section-well col-sm-offset-1 col-sm-4">
           <ListGroupItem header="Real Projects" href="#" bsClass="list-group-item landing-page-markers">
             Satisfy real charity customers with open source code for great causes around the world.
           </ListGroupItem>
           </div>
-          <div class="section-well col-sm-8 pull-right">
+          <div class="section-well col-sm-offset-7 col-sm-4">
           <ListGroupItem header="Sprints" href="#" bsClass="list-group-item landing-page-markers">
             Now you've got yourself introduced and interested in a project, why not commit to a sprint or two?
           </ListGroupItem>
           </div>
-          <div class="section-well col-sm-8 pull-right">
+          <div class="section-well col-sm-offset-1 col-sm-4">
           <ListGroupItem header="Jobs" href="#" bsClass="list-group-item landing-page-markers">
             Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.
           </ListGroupItem>
