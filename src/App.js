3a9e32b4be58c0f1a1cc83ee2f7071f91d9184c4
SherSpock <wrtimberlake@gmail.com>
2018-04-13 06:59:58 -0400

    Refactor vanilla bootstrap elements into react bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Grid, Col, Row, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 import logo from './logo.png';
 import AVNavbar from './AVNavbar';
@@ -18,34 +18,51 @@ class App extends Component {
         <AVNavbar/>
 
         <ListGroup>
-          <div class="section-well col-sm-offset-1 col-sm-4">
-          <p class='landing-page-markers you-are-here'>You are here!</p>
-          {/* <ListGroupItem header="You are here" href="#" bsClass="list-group-item landing-page-markers">
-              Tired of toy projects, tutorials, and online courses?
-          </ListGroupItem> */}
-          </div>
-          <div class="section-well col-sm-offset-7 col-sm-4">
-          <ListGroupItem header="Standups" href="#" bsClass="list-group-item landing-page-markers">
-            Sitting down at home, or not, meet other people in our online hangouts.
-          </ListGroupItem>
-          </div>
-          <div class="section-well col-sm-offset-1 col-sm-4">
-          <ListGroupItem header="Real Projects" href="#" bsClass="list-group-item landing-page-markers">
-            Satisfy real charity customers with open source code for great causes around the world.
-          </ListGroupItem>
-          </div>
-          <div class="section-well col-sm-offset-7 col-sm-4">
-          <ListGroupItem header="Sprints" href="#" bsClass="list-group-item landing-page-markers">
-            Now you've got yourself introduced and interested in a project, why not commit to a sprint or two?
-          </ListGroupItem>
-          </div>
-          <div class="section-well col-sm-offset-1 col-sm-4">
-          <ListGroupItem header="Jobs" href="#" bsClass="list-group-item landing-page-markers">
-            Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.
-          </ListGroupItem>
-          </div>
-        </ListGroup>;
-
+          <Row>
+            <Col xsOffset={1} sm={4} class="section-well">
+            
+              <p class='landing-page-markers you-are-here'>You are here!</p>
+              {/* <ListGroupItem header="You are here" href="#" bsClass="list-group-item landing-page-markers">
+                  Tired of toy projects, tutorials, and online courses?
+              </ListGroupItem> */}
+            </Col>
+          </Row>
+          <Row>
+            <Col xsOffset={7} sm={4} class="section-well">
+              <ListGroupItem header="Standups" href="#" bsClass="list-group-item landing-page-markers">
+                Sitting down at home, or not, meet other people in our online hangouts.
+              </ListGroupItem>
+            </Col>
+          </Row>
+          <Row>
+            <Col xsOffset={1} sm={4} class="section-well">
+              <ListGroupItem header="Real Projects" href="#" bsClass="list-group-item landing-page-markers">
+                Satisfy real charity customers with open source code for great causes around the world.
+              </ListGroupItem>
+            </Col>
+          </Row>  
+          <Row>
+            <Col xsOffset={7} sm={4} class="section-well">
+              <ListGroupItem header="Sprints" href="#" bsClass="list-group-item landing-page-markers">
+                Now you've got yourself introduced and interested in a project, why not commit to a sprint or two?
+              </ListGroupItem>
+            </Col>
+          </Row>
+          <Row>
+            <Col xsOffset={1} sm={4} class="section-well">
+              <ListGroupItem header="Jobs" href="#" bsClass="list-group-item landing-page-markers">
+                Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.
+              </ListGroupItem>  
+            </Col>
+          </Row>   
+          <Row>
+            <Col xsOffset={1} sm={4} class="section-well">
+              <ListGroupItem header="Jobs" href="#" bsClass="list-group-item landing-page-markers">
+                Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.
+              </ListGroupItem>
+            </Col>
+          </Row>    
+        </ListGroup>
       </div>
     );
   }
