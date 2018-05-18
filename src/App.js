43649579db603716889508a77f7abed831e218a4
Matt Rider <mattwr18@gmail.com>
2018-05-18 10:50:22 -0300

    Rotate 180 background image, reposition bootstrap wells
    For the user to move in the direction of the beach instead of away from.

--- src/App.js
+++ src/App.js
@@ -30,28 +30,28 @@ class App extends Component {
           </ScrollAnimation>
           <ScrollAnimation animateIn="tada" offset={10} delay={600} animateOnce={true}>
           <Row>
-            <Col xsOffset={6} sm={5} class="section-well">
+            <Col xsOffset={0} mdPull={2} sm={5} class="section-well">
               <p class='landing-page-markers you-are-here'>Standups: Sitting down at home, or not, meet other people in our online hangouts.</p>
             </Col>
           </Row>
           </ScrollAnimation>
           <ScrollAnimation animateIn="tada" offset={10} delay={700} animateOnce={true}>
           <Row>
-            <Col xsOffset={1} sm={5} class="section-well">
+            <Col xsOffset={3} sm={5} class="section-well">
               <p class='landing-page-markers you-are-here'>Real Projects: Satisfy real charity customers with open source code for great causes around the world.</p>
             </Col>
           </Row>
           </ScrollAnimation>
           <ScrollAnimation animateIn="tada" offset={10} delay={800} animateOnce={true}>
           <Row>
-            <Col xsOffset={6} sm={5} class="section-well">
+            <Col xsOffset={8} sm={5} class="section-well">
               <p class='landing-page-markers you-are-here'>Sprints: Now you've got yourself introduced and interested in a project, why not commit to a sprint or two?</p>
             </Col>
           </Row>
           </ScrollAnimation>
           <ScrollAnimation animateIn="tada" offset={10} animateOnce={true}>
           <Row>
-            <Col xsOffset={1} sm={5} class="section-well">
+            <Col xsOffset={6} sm={5} class="section-well">
               <p class='landing-page-markers you-are-here'>Jobs: Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.</p>
             </Col>
           </Row>
