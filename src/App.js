741d857002491a26f5a1006196ad505141bf56ae
Sam Joseph <tansaku@gmail.com>
2018-06-15 13:05:03 +0100

    removing animation and starting to style modal

--- src/App.js
+++ src/App.js
@@ -42,75 +42,70 @@ class App extends Component {
         <AVNavbar />
 
         <Grid>
-          <ScrollAnimation animateIn="tada" offset={10} delay={1000} animateOnce={true}>
-            <Row>
-              <Col xsOffset={1} sm={5} className="section-well">
-                <Button bsStyle="primary" bsSize="large">
-                  <p>You are here!
+          <Row>
+            <Col xsOffset={1} sm={5} className="section-well">
+              <Button bsStyle="primary" bsSize="large">
+                <p>You are here!
                     <img src={logo} className="App-logo" alt="logo" />
-                  </p>
-                </Button>
-              </Col>
-            </Row>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="tada" offset={10} delay={3000} animateOnce={true}>
-            <Row>
-              <Col xsOffset={0} mdPull={2} sm={5} style={{ width: 400, margin: '100px 0' }}>
-                <ReactHover>
-                  <ReactHover.Trigger type='trigger'>
-                    <Button bsStyle="primary" bsSize="large">
-                      <h1>Standups</h1>
-                    </Button>
-                  </ReactHover.Trigger>
-                  <ReactHover.Hover type='hover'>
-                    <p className="landing-page-markers you-are-here">Sitting down at home, or not, meet other people in our online hangouts.</p>
-                  </ReactHover.Hover>
-                </ReactHover>
-              </Col>
-            </Row>
-          </ScrollAnimation>
+                </p>
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col xsOffset={0} mdPull={2} sm={5} style={{ width: 400, margin: '100px 0' }}>
+              <ReactHover>
+                <ReactHover.Trigger type='trigger'>
+                  <Button bsStyle="primary" bsSize="large">
+                    <h1>Standups</h1>
+                  </Button>
+                </ReactHover.Trigger>
+                <ReactHover.Hover type='hover'>
+                  <p className="landing-page-markers you-are-here">Sitting down at home, or not, meet other people in our online hangouts.</p>
+                </ReactHover.Hover>
+              </ReactHover>
+            </Col>
+          </Row>
           <Row>
             <Col xsOffset={3} sm={5} className="section-well">
               <div>
                 <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
                   <h1>Real Projects</h1>
                 </Button>
-                <Modal show={this.state.show} onHide={this.handleClose}>
+                <Modal
+                  dialogClassName="orange-background"
+                  show={this.state.show} onHide={this.handleClose}
+                >
                   <Modal.Header closeButton>
                     <Modal.Title>Real Projects</Modal.Title>
                   </Modal.Header>
-                  <Modal.Body>
+                  <Modal.Body componentClass="orange-background">
                     <p className='landing-page-markers you-are-here'>Satisfy real charity customers with open source code for great causes around the world.
                       <img src={logo} className="App-logo" alt="logo" />
                     </p>
                   </Modal.Body>
-                  <Modal.Footer>
+                  {/* <Modal.Footer>
                     <Button onClick={this.handleClose}>Close</Button>
-                  </Modal.Footer>
+                  </Modal.Footer> */}
                 </Modal>
               </div>
             </Col>
           </Row>
-          <ScrollAnimation animateIn="tada" offset={10} delay={1000} animateOnce={true}>
-            <Row>
-              <Col xsOffset={8} sm={5} className="section-well">
-                <p className='landing-page-markers you-are-here'>Sprints{/*: Now you have got yourself introduced and interested in a project, why not commit to a sprint or two? */}
+          <Row>
+            <Col xsOffset={8} sm={5} className="section-well">
+              <p className='landing-page-markers you-are-here'>Sprints{/*: Now you have got yourself introduced and interested in a project, why not commit to a sprint or two? */}
+                <img src={logo} className="App-logo" alt="logo" />
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col xsOffset={6} sm={5}>
+              <Well>
+                <p className='landing-page-markers you-are-here'>Jobs{/*: Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.*/}
                   <img src={logo} className="App-logo" alt="logo" />
                 </p>
-              </Col>
-            </Row>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="tada" offset={10} delay={3000} animateOnce={true}>
-            <Row>
-              <Col xsOffset={6} sm={5}>
-                <Well>
-                  <p className='landing-page-markers you-are-here'>Jobs{/*: Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.*/}
-                    <img src={logo} className="App-logo" alt="logo" />
-                  </p>
-                </Well>
-              </Col>
-            </Row>
-          </ScrollAnimation>
+              </Well>
+            </Col>
+          </Row>
         </Grid>
 
 
