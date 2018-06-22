9e7498eb03496ac411f3f4cfd978347a87768a86
Sam Joseph <tansaku@gmail.com>
2018-06-22 13:56:15 +0100

    all modals working independently

--- src/App.js
+++ src/App.js
@@ -18,22 +18,16 @@ class App extends Component {
     this.handleClose = this.handleClose.bind(this);
 
     this.state = {
-      here: false,
-      standups: false,
-      projects: false
+      show: null
     };
   }
 
   handleClose(id) {
-    this.setState({ here: false });
-    // this.setState({ standups: false });
-    // this.setState({ projects: false });
+    this.setState({show: id});
   }
 
   handleShow(id) {
-     this.setState({ here: true });
-     // this.setState({ standups: true });
-     // this.setState({ projects: true });
+    this.setState({show: id});
   }
 
   render() {
@@ -50,11 +44,11 @@ class App extends Component {
         <Grid>
           <Row>
             <Col xsOffset={1} sm={5}>
-              <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+              <Button bsStyle="primary" bsSize="large" onClick={() => this.handleShow('here')}>
                 <h1>You are here!</h1>
               </Button>
               <Modal
-                show={this.state.here} onHide={this.handleClose}
+                show={this.state.show == 'here'} onHide={this.handleClose}
               >
                 <Modal.Header closeButton closeLabel="close window">
                 </Modal.Header>
@@ -68,11 +62,11 @@ class App extends Component {
           </Row>
           <Row>
             <Col xsOffset={0} mdPull={2} sm={5}>
-              <Button bsStyle="primary" bsSize="large" onClick={this.handleShow, 'standups'}>
+              <Button bsStyle="primary" bsSize="large" onClick={() => this.handleShow('standups')}>
                 <h1>Standups</h1>
               </Button>
               <Modal
-                show={this.state.standups} onHide={this.handleClose}
+                show={this.state.show == 'standups'} onHide={this.handleClose}
               >
                 <Modal.Header closeButton closeLabel="close window">
                 </Modal.Header>
@@ -86,11 +80,11 @@ class App extends Component {
           </Row>
           <Row>
             <Col xsOffset={3} sm={5}>
-              <Button bsStyle="primary" bsSize="large" onClick={this.handleShow, 'projects'}>
+              <Button bsStyle="primary" bsSize="large" onClick={() => this.handleShow('projects')}>
                 <h1>Real Projects</h1>
               </Button>
               <Modal
-                show={this.state.projects} onHide={this.handleClose}
+                show={this.state.show == 'projects'} onHide={this.handleClose}
               >
                 <Modal.Header closeButton closeLabel="close window">
                 </Modal.Header>
@@ -104,18 +98,38 @@ class App extends Component {
           </Row>
           <Row>
             <Col xsOffset={8} sm={5}>
-              <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+              <Button bsStyle="primary" bsSize="large" onClick={() => this.handleShow('sprints')}>
                 <h1>Sprints</h1>
               </Button>
-              {/*: Now you have got yourself introduced and interested in a project, why not commit to a sprint or two? */}
+              <Modal
+                show={this.state.show == 'sprints'} onHide={this.handleClose}
+              >
+                <Modal.Header closeButton closeLabel="close window">
+                </Modal.Header>
+                <Modal.Body>
+                  <p className='landing-page-markers you-are-here'>Now you have got yourself introduced and interested in a project, why not commit to a sprint or two?
+                      <img src={logo} className="App-logo" alt="logo" />
+                  </p>
+                </Modal.Body>
+              </Modal>
             </Col>
           </Row>
           <Row>
             <Col xsOffset={6} sm={5}>
-              <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+              <Button bsStyle="primary" bsSize="large" onClick={() => this.handleShow('jobs')}>
                 <h1>Jobs</h1>
               </Button>
-              {/*: Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.*/}
+              <Modal
+                show={this.state.show == 'jobs'} onHide={this.handleClose}
+              >
+                <Modal.Header closeButton closeLabel="close window">
+                </Modal.Header>
+                <Modal.Body>
+                  <p className='landing-page-markers you-are-here'> Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.
+                      <img src={logo} className="App-logo" alt="logo" />
+                  </p>
+                </Modal.Body>
+              </Modal>
             </Col>
           </Row>
         </Grid>
