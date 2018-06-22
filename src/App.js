912ae0ddd75841d7a874404fc151f912bc283c08
Sam Joseph <tansaku@gmail.com>
2018-06-22 13:07:04 +0100

    standardize styles - now need to have different buttons open different modals

--- src/App.js
+++ src/App.js
@@ -43,71 +43,76 @@ class App extends Component {
 
         <Grid>
           <Row>
-            <Col xsOffset={1} sm={5} className="section-well">
-              <Button bsStyle="primary" bsSize="large">
-                <p>You are here!
-                    <img src={logo} className="App-logo" alt="logo" />
-                </p>
+            <Col xsOffset={1} sm={5}>
+              <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+                <h1>You are here!</h1>
               </Button>
-            </Col>
-          </Row>
-          <Row>
-            <Col xsOffset={0} mdPull={2} sm={5} style={{ width: 400, margin: '100px 0' }}>
-              <ReactHover>
-                <ReactHover.Trigger type='trigger'>
-                  <Button bsStyle="primary" bsSize="large">
-                    <h1>Standups</h1>
-                  </Button>
-                </ReactHover.Trigger>
-                <ReactHover.Hover type='hover'>
-                  <p className="landing-page-markers you-are-here">Sitting down at home, or not, meet other people in our online hangouts.</p>
-                </ReactHover.Hover>
-              </ReactHover>
-            </Col>
-          </Row>
-          <Row>
-            <Col xsOffset={3} sm={5} className="section-well">
-              <div>
-                <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
-                  <h1>Real Projects</h1>
-                </Button>
-                <Modal
-                  show={this.state.show} onHide={this.handleClose}
-                >
-                  <Modal.Header closeButton closeLabel="close window">
-                  </Modal.Header>
-                  <Modal.Body>
-                    <p className='landing-page-markers you-are-here'>Satisfy real charity customers with open source code for great causes around the world.
+              <Modal
+                show={this.state.show} onHide={this.handleClose}
+              >
+                <Modal.Header closeButton closeLabel="close window">
+                </Modal.Header>
+                <Modal.Body>
+                  <p className='landing-page-markers you-are-here'>Tired of toy projects, tutorials and online courses?
                       <img src={logo} className="App-logo" alt="logo" />
-                    </p>
-                  </Modal.Body>
-                  {/* <Modal.Footer>
-                    <Button onClick={this.handleClose}>Close</Button>
-                  </Modal.Footer> */}
-                </Modal>
-              </div>
+                  </p>
+                </Modal.Body>
+              </Modal>
             </Col>
           </Row>
           <Row>
-            <Col xsOffset={8} sm={5} className="section-well">
-              <p className='landing-page-markers you-are-here'>Sprints{/*: Now you have got yourself introduced and interested in a project, why not commit to a sprint or two? */}
-                <img src={logo} className="App-logo" alt="logo" />
-              </p>
+            <Col xsOffset={0} mdPull={2} sm={5}>
+              <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+                <h1>Standups</h1>
+              </Button>
+              <Modal
+                show={this.state.show} onHide={this.handleClose}
+              >
+                <Modal.Header closeButton closeLabel="close window">
+                </Modal.Header>
+                <Modal.Body>
+                  <p className='landing-page-markers you-are-here'>Sitting down at home, or not, meet other people in our online hangouts.
+                      <img src={logo} className="App-logo" alt="logo" />
+                  </p>
+                </Modal.Body>
+              </Modal>
+            </Col>
+          </Row>
+          <Row>
+            <Col xsOffset={3} sm={5}>
+              <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+                <h1>Real Projects</h1>
+              </Button>
+              <Modal
+                show={this.state.show} onHide={this.handleClose}
+              >
+                <Modal.Header closeButton closeLabel="close window">
+                </Modal.Header>
+                <Modal.Body>
+                  <p className='landing-page-markers you-are-here'>Satisfy real charity customers with open source code for great causes around the world.
+                      <img src={logo} className="App-logo" alt="logo" />
+                  </p>
+                </Modal.Body>
+              </Modal>
+            </Col>
+          </Row>
+          <Row>
+            <Col xsOffset={8} sm={5}>
+              <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+                <h1>Sprints</h1>
+              </Button>
+              {/*: Now you have got yourself introduced and interested in a project, why not commit to a sprint or two? */}
             </Col>
           </Row>
           <Row>
             <Col xsOffset={6} sm={5}>
-              <Well>
-                <p className='landing-page-markers you-are-here'>Jobs{/*: Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.*/}
-                  <img src={logo} className="App-logo" alt="logo" />
-                </p>
-              </Well>
+              <Button bsStyle="primary" bsSize="large" onClick={this.handleShow}>
+                <h1>Jobs</h1>
+              </Button>
+              {/*: Premium members can get compensated for their time on paid projects, and many other alumni have gone on to great things in the wider world.*/}
             </Col>
           </Row>
         </Grid>
-
-
-
       </div >
     );
   }
