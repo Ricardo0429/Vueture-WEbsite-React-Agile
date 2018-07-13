73ba939194272a71d8be9df9ecb1726b1a6761fd
Matt Rider <mattwr18@gmail.com>
2018-07-13 09:31:51 -0300

    Continue extracting modals into function

--- src/App.js
+++ src/App.js
@@ -22,10 +22,26 @@ class App extends Component {
     this.modals = [
       {
         buttonText: 'You are here!',
+        xsOffset: 1,
         reactId: 'here',
-        imageName: 'ladydev',
+        image: ladydev,
         modalText: 'Tired of toy projects, tutorials and online courses?'
-      }
+      },
+      {
+        buttonText: 'Standups',
+        xsOffset: 0,
+        mdPull: 2,
+        reactId: 'standups',
+        image: scrum,
+        modalText: 'Sitting down at home, or not, meet other people in our online hangouts.'
+      },
+      {
+        buttonText: 'Real Projects',
+        xsOffset: 3,
+        reactId: 'projects',
+        image: realprojects,
+        modalText: 'Satisfy real charity customers with open source code for great causes around the world.'
+      },
     ]
   }
 
@@ -41,25 +57,24 @@ class App extends Component {
     return this.modals.map(modal => {
       return (
         <Row>
-        <Col xsOffset={1} sm={5}>
-          <Button bsStyle="primary" bsSize="large" block onClick={() => this.handleShow('here')}>
+        <Col xsOffset={modal.xsOffset} sm={5} mdPull={modal.mdPull}>
+          <Button bsStyle="primary" bsSize="large" block onClick={() => this.handleShow(modal.reactId)}>
             <h1>{modal.buttonText}</h1>
           </Button>
           <Modal
-            show={this.state.show == 'here'} onHide={this.handleClose}
+            show={this.state.show == modal.reactId} onHide={this.handleClose}
           >
             <Modal.Header closeButton closeLabel="close window">
             </Modal.Header>
             <Modal.Body>
-              <img src={ladydev} alt="lady dev" className="lady-dev" />
-              <p className='landing-page-markers you-are-here'>Tired of toy projects, tutorials and online courses?
-              </p>
+              <img src={modal.image} alt="lady dev" className="lady-dev" />
+              <p className='landing-page-markers you-are-here'>{modal.modalText}</p>
             </Modal.Body>
           </Modal>
         </Col>
       </Row>
       )
-    });  
+    });
   }
 
   render() {
@@ -77,42 +92,7 @@ class App extends Component {
 
           {/* loop */}
           {this.renderRows()}
-          <Row>
-            <Col xsOffset={0} mdPull={2} sm={5}>
-              <Button bsStyle="primary" bsSize="large" block onClick={() => this.handleShow('standups')}>
-                <h1>Standups</h1>
-              </Button>
-              <Modal
-                show={this.state.show == 'standups'} onHide={this.handleClose}
-              >
-                <Modal.Header closeButton closeLabel="close window">
-                </Modal.Header>
-                <Modal.Body>
-                  <img src={scrum} className="lady-dev" alt="logo" />
-                  <p className='landing-page-markers you-are-here'>Sitting down at home, or not, meet other people in our online hangouts.
-                  </p>
-                </Modal.Body>
-              </Modal>
-            </Col>
-          </Row>
-          <Row>
-            <Col xsOffset={3} sm={5}>
-              <Button bsStyle="primary" bsSize="large" block onClick={() => this.handleShow('projects')}>
-                <h1>Real Projects</h1>
-              </Button>
-              <Modal
-                show={this.state.show == 'projects'} onHide={this.handleClose}
-              >
-                <Modal.Header closeButton closeLabel="close window">
-                </Modal.Header>
-                <Modal.Body>
-                  <img src={realprojects} className="real-projects" alt="logo" />
-                  <p className='landing-page-markers you-are-here'>Satisfy real charity customers with open source code for great causes around the world.
-                  </p>
-                </Modal.Body>
-              </Modal>
-            </Col>
-          </Row>
+
           <Row>
             <Col xsOffset={8} sm={5}>
               <Button bsStyle="primary" bsSize="large" block onClick={() => this.handleShow('sprints')}>
