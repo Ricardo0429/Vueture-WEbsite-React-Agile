e863cb8a449b404f0c358c2a15acd905f3e9c1cb
Matt Rider <mattwr18@gmail.com>
2018-08-24 08:55:26 -0300

    Add snapshot, button test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import AVNavbar from './components/AVNavbar';
+import Button from 'react-bootstrap';
 
 it('renders without crashing', () => {
   shallow(<App />);
@@ -12,3 +13,8 @@ it('renders a AVNavbar', () => {
   const wrapper = shallow(<App />);
   expect(wrapper.find(AVNavbar).length).toEqual(1);
 });
+
+// it('renders a Button', () => {
+//   const wrapper = shallow(<App />);
+//   expect(wrapper.find(Button).length).toEqual(5);
+// });
--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -3,7 +3,25 @@ import { shallow } from 'enzyme';
 import Button from '../../App.js';
 
 describe('Button', () => {
-  it('renders without crashing', () => {
-    shallow(<Button />);
+  it('should be defined', () => {
+   expect(Button).toBeDefined();
   });
+
+  it('should render correctly', () => {
+   const tree = shallow(
+     <Button name='button test' />
+   );
+   expect(tree).toMatchSnapshot();
+  });
+
+  // it('renders without crashing', () => {
+  //   shallow(<Button />);
+  // });
+
+  it('should have a button value', () => {
+   const tree = shallow(
+     <Button name='button test' />
+   );
+   expect(tree.getElement('.button').value).toEqual('button test');
+ });
 });
