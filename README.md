73a1641075576a71750884e95677736107b308ff
Daniel Kukula <daniel.kuku@gmail.com>
2018-11-26 14:11:15 +0000

    13 events calendar (#14)
    * Update README.md
    
    * Update README.md
    
    * add event calendar to events page
    
    * Show events in calendar
    
    * events showing up in calendar
    
    * use spread operator for events
    
    * style calandar month view
    
    * set up local development proxy

--- README.md
+++ README.md
@@ -20,5 +20,34 @@ Run
 ```
 $ yarn start
 ```
- 
- We recently had issues with `yarn test` on macs, which were solved with `brew install watchman`
+Dokku Deployment
+-----------------
+
+This will only work if you have been given access to our dokku system - this is for advanced users who have demonstrated ongoing committment to the project.
+
+To make the commands simple, add the following to your ~/.ssh/config
+
+```
+Host avp-dokku
+HostName agileventures-playground.westeurope.cloudapp.azure.com
+User dokku
+```
+Then the following commands (assuming you have the correct ssh key set up) will create an app and allow pushing to the production server via git
+
+```sh
+$ ssh avp-dokku apps:create agileventures-react-front-end-production # if not yet created
+$ git remote add agileventures-react-front-end-production dokku@agileventures-playground.westeurope.cloudapp.azure.com:agileventures-react-front-end-production    # assuming you are in local directory for this project -- only needed first time
+$ git push agileventures-react-front-end-production master
+```
+
+Setting up a Domain on Dokku/Azure
+----------------------------
+
+Tell dokku what domain to use:
+```
+$ ssh avp-dokku domains:add agileventures-react-front-end-production react.agileventures.org
+```
+
+Ensure has the DNS Zone on Azure is set correctly:
+
+![Azure DNS Zone configuration pane](https://dl.dropbox.com/s/dzgh7i3sn4wrxqz/Screenshot%202018-11-01%2013.32.30.png?dl=0)
--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { Card, Container } from "semantic-ui-react";
+import BigCalendar from "react-big-calendar";
 import axios from "axios";
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+
 
 export default class EventsList extends Component {
   constructor(props) {
@@ -15,10 +20,16 @@ export default class EventsList extends Component {
     //todo remove this when we start using redux
     this.mounted = true;
 
-    axios.get("https://www.agileventures.org/events.json").then(response => {
-      console.log(response.data);
+    axios.get("events.json").then(response => {
       if (this.mounted) {
-        this.setState({ events: response.data });
+          let events=response.data.map(event => {
+            return {
+              ...event,
+              start: new Date(event.start),
+              end: new Date(event.end),
+            }
+          })
+        this.setState({ events });
       }
     });
   }
@@ -28,23 +39,30 @@ export default class EventsList extends Component {
     this.mounted = false;
   }
   render() {
+    console.log(this.state.events)
+    const localizer = BigCalendar.momentLocalizer(moment)
     return (
       <Container>
-        <Card.Group>
-          {this.state.events.map((event, id) => {
-            return (
-              <Card key={id}>
-                <Card.Content>
-                  <Card.Header>{event.title}</Card.Header>
-                  <Card.Meta>
-                  {new Date(event.start).toUTCString()} - {new Date(event.end).toUTCString().substring(17)}
-                  </Card.Meta>
-                </Card.Content>
-              </Card>
-            );
-          })}
-        </Card.Group>
-      </Container>
+        <BigCalendar
+          localizer={localizer}
+          events={this.state.events}
+    />
+
+  <Card.Group>
+    {this.state.events.map((event, id) => {
+      return (
+        <Card key={id}>
+          <Card.Content>
+            <Card.Header>{event.title}</Card.Header>
+            <Card.Meta>
+              {moment(event.start).format('MM-DD hh:mm')} - {moment(event.end).format('MM-DD hh:mm')}
+        </Card.Meta>
+      </Card.Content>
+    </Card>
+      );
+    })}
+      </Card.Group>
+    </Container>
     );
   }
 }
