0d932c62bad2e67b5bed4fc6e650d8b90f83447c
Matt Rider <mattwr18@gmail.com>
2018-11-29 17:25:00 -0200

    Remove proxy

--- src/actions/getEventsAction.js
+++ src/actions/getEventsAction.js
@@ -4,7 +4,8 @@ import { GET_EVENTS } from "../types";
 export let getEvents = events => ({ type: GET_EVENTS, payload: events });
 
 export let fetchEvents = () => dispatch => {
-  return axios.get("/events.json").then(response => {
+  return axios.get("https://www.agileventures.org/events.json").then(response => {
+    console.log(response);
     dispatch(getEvents(response.data));
   });
 };
